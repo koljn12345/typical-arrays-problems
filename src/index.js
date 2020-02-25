4a59a549baf81c2d41f29adf29d561652fae99d1
koljn12345 <litovchenko_kolj@mail.ru>
2020-02-25 17:42:31 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length===0) return 0;
+  return array.reduce((p, el)=>p= el<p? el: p, array[0])
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length===0) return 0;
+  return array.reduce((p, el)=>p= el>p? el: p, array[0])
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length===0) return 0;
+  const summ= array.reduce((p, el)=> p += el, 0);
+  return summ/array.length;
 }
